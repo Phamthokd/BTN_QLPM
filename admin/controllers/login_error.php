8594803dfb1068406cd7d91c1eed1ece712c98b3
JRabbit <ledinhtho686@gmail.com>
2022-05-15 10:05:16 +0700

    sửa đăng nhập cho admin

--- admin/controllers/login_error.php
+++ admin/controllers/login_error.php
@@ -1,5 +1,5 @@
 <?php
 include('session_start.php');
 if(!empty($_SESSION['login_error'])) {
-    echo '<div class="alert alert-warning" role="alert">username or password is not correct</div>';
+    echo '<div class="alert alert-warning" role="alert">Tên đăng nhập hoặc mật khẩu không đúng<br>Vui lòng nhập lại!</div>';
 }
